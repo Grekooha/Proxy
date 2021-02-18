2d0cc43a1767b976f202da6990a1993ab1dabe46
kkochukov <darkcrusider@gmail.com>
2021-02-18 20:45:42 +0300

    Parallel proxy processing realization

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,9 +5,12 @@ public class Main {
     public static void main(String[] args) {
 
 //        ProxyHelper.findFastestProxy(Parsing.getProxies());
-
-        ProxyData fast = ProxyHelper.findFastestProxy(Parser.getProxies());
-        System.out.println("Result " + fast.getHost() + " " + fast.getDelay());
+        var ph = new ProxyHelper();
+        var t1 = System.currentTimeMillis();
+        var fast = ph.findFastestProxy(Parser.getProxies());
+        var t2 = System.currentTimeMillis();
+        System.out.println(t2 - t1);
+        System.out.println("Result " + fast.get().getHost() + " " + fast.get().getDelay());
     }
 }
 
--- src/com/company/ProxyHelper.java
+++ src/com/company/ProxyHelper.java
@@ -3,39 +3,63 @@ package com.company;
 import java.io.IOException;
 import java.net.*;
 import java.util.ArrayList;
+import java.util.Optional;
 
 public class ProxyHelper {
 
     private static final String RESOURCE__URL = "http://rutracker.org";
 
-    public static ProxyData findFastestProxy(ArrayList<ProxyData> proxies) {
+    private Integer counter = 0;
+    private Optional<ProxyData> fastestProxy = Optional.empty();
+
+    public Optional<ProxyData> findFastestProxy(ArrayList<ProxyData> proxies) {
         System.out.println("Total proxies in the list: " + proxies.size());
         System.out.println("==================");
-        ProxyData fastProxy = null;
-        for (ProxyData entry : proxies)
-            try {
-                SocketAddress sa = new InetSocketAddress(entry.getHost(), entry.getPort());
-                Proxy proxy = new Proxy(entry.getType(), sa);
-                URL url = new URL(RESOURCE__URL);
-                URLConnection conn = url.openConnection(proxy);
-                conn.setConnectTimeout(100);
-                long timeStart = System.currentTimeMillis();
-                conn.connect();
-                int delay = (int) (System.currentTimeMillis() - timeStart);
-                if (fastProxy == null) {
-                    entry.setDelay(delay);
-                    fastProxy = entry;
-                } else if (fastProxy.getDelay() > delay) {
-                    entry.setDelay(delay);
-                    fastProxy = entry;
-                }
-
-            } catch (IOException e) {
-                e.getMessage();
-            }
+        proxies.parallelStream().forEach(this::checkProxy);
+        waitResult(proxies);
         System.out.println("Done.");
         System.out.println("=================");
-        return fastProxy;
+        return fastestProxy;
     }
 
+    private void waitResult(ArrayList<ProxyData> proxies) {
+        var timeStart = System.currentTimeMillis();
+        while (counter < proxies.size()) {
+            int MAX_AWAIT_MS = 60000;
+            if ( System.currentTimeMillis() - timeStart > MAX_AWAIT_MS) break;
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    private void checkProxy(ProxyData entry) {
+        try {
+            SocketAddress address = new InetSocketAddress(entry.getHost(), entry.getPort());
+            URLConnection conn = new URL(RESOURCE__URL)
+                    .openConnection(new Proxy(entry.getType(), address));
+            conn.setConnectTimeout(100);
+            long timeStart = System.currentTimeMillis();
+            conn.connect();
+            int delay = (int) (System.currentTimeMillis() - timeStart);
+            synchronized (fastestProxy) {
+                if (fastestProxy.isEmpty()) {
+                    entry.setDelay(delay);
+                    fastestProxy = Optional.of(entry);
+                } else if (fastestProxy.get().getDelay() > delay) {
+                    entry.setDelay(delay);
+                    fastestProxy = Optional.of(entry);
+                }
+            }
+            synchronized (counter) {
+                counter++;
+            }
+        } catch ( IOException e) {
+            synchronized (counter) {
+                counter++;
+            }
+        }
+    }
 }
